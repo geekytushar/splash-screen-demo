d8c23c67e40e4c1965a91c89332d95c6f8c07357
Tushar Patil <tushar@Tushars-MacBook.local>
2018-06-14 21:55:31 +0530

    First commit

---
+++ app/src/main/java/com/tusharpatil/splashscreendemo/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.tusharpatil.splashscreendemo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //redirect to main activity
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
